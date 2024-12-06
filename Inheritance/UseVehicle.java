a0813eaa4ad647ba5141e2ff59bb669464f5b50a
SachinDevTech <sachin.1107sk@gmail.com>
2024-12-06 17:28:10 +0530

    Add single level Inheritance

---
+++ Inheritance/UseVehicle.java
@@ -0,0 +1,14 @@
+// package Inheritance;
+
+public class UseVehicle {
+
+    public static void main(String[] args) {
+        Car bmw = new Car();
+        //Example of single level inheritance
+        System.out.println("Single Level Inheritance Example...");
+        bmw.turnLeft();
+        bmw.turnRight();
+        bmw.starts();
+        bmw.accelerate();
+    }
+}
---
+++ Inheritance/Vehicle.java
@@ -0,0 +1,21 @@
+// package Inheritance;
+
+public class Vehicle {
+
+    public void starts() {
+        System.out.println("Starting...");
+    }
+
+    public void accelerate() {
+        System.out.println("Accelerating...");
+    }
+
+    public void turnRight() {
+        System.out.println("turning right...");
+    }
+
+    public void turnLeft() {
+        System.out.println("turning left...");
+    }
+
+}
